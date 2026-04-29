8b0f81ddc7f04103107cca2e9d54a8248f7e7559
Yelyzaveta Aksionova <135411053+lisaaksionova@users.noreply.github.com>
2026-04-29 22:33:43 +0300

    Feature CATALOG API (#3)
    * initial project setup
    
    * add model
    
    * add global usings
    
    * add minimal endpoints
    
    * configure program file

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    string Description,
+    List<string> Category,
+    string ImageFile,
+    decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // business logic
+        
+        // create product
+        var product = new Product
+        {
+            Name = command.Name,
+            Description = command.Description,
+            Category = command.Category,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // save to db
+
+        // return result
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
